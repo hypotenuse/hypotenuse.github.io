5e1445bdf5e820d6cb52b50338d054c901b74f5d
Ivan <hypotenuse2@gmail.com>
2019-09-29 20:01:11 +0300

    ап

--- js/welcome.js
+++ js/welcome.js
@@ -6,6 +6,7 @@
 
     let s1 = document.getElementById('s1')
     let s2 = document.getElementById('s2')
+    let s3 = document.getElementById('s3')
 
     let briefSection = document.getElementById('brief-section')
     let educationSection = document.getElementById('education-section')
@@ -13,12 +14,14 @@
     
     const delay1 = 250 * 2
     const delay2 = 250 * 12
+    const delay3 = 250 * 22
 
     setTimeout(() => {
         s1.style.opacity = '1'
         header.style.opacity = '1'
     }, delay1)
     setTimeout(() => {s2.style.opacity = '1'}, delay2)
+    setTimeout(() => {s3.style.opacity = '1'}, delay3)
 
     setTimeout(() => {
         briefSection.style.opacity = '1'
